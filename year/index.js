a0da761a960057ddf5d99e18c92aae9a94ec61f0
yuto mori <yutomori1979@gmail.com>
2018-02-12 10:17:19 +0900

    year にwiki情報を追加

--- year/index.js
+++ year/index.js
@@ -5,7 +5,7 @@ const START_YEAR = 1970;
 //const ONE_MINUTE = 60;
 //const ONE_HOUR = 60;
 //const ONE_DAY = 24;
-const ONE_YEAR = 31556925168;
+const ONE_YEAR = 31556925168;//wikiを参考 https://ja.wikipedia.org/wiki/%E5%A4%AA%E9%99%BD%E5%B9%B4
 
 function year(d){
     return  d  / ONE_YEAR + START_YEAR;
